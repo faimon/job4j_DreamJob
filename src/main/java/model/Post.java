526c186fffc52bfbc69e6b50fcd29436201a575a
mac <faimon96@gmail.com>
2020-10-24 10:23:45 +0700

    2. Scriplet. Динамическая таблица. [#372938]

---
+++ src/main/java/model/Post.java
@@ -0,0 +1,66 @@
+package model;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Post {
+    private int id;
+    private String name;
+    private String description;
+    private LocalDate date;
+
+    public Post(int id, String name, String description, LocalDate date) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Post post = (Post) o;
+        return id == post.id &&
+                Objects.equals(name, post.name) &&
+                Objects.equals(description, post.description) &&
+                Objects.equals(date, post.date);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, description, date);
+    }
+}
