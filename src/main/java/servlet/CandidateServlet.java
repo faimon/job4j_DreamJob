0c4a4ae453ad1a7933030ddac4abd4989d7adc47
mac <faimon96@gmail.com>
2020-10-28 19:23:25 +0700

    2. Замена MemStore на PsqlStore [#372918]

--- src/main/java/servlet/CandidateServlet.java
+++ src/main/java/servlet/CandidateServlet.java
@@ -2,6 +2,7 @@ package servlet;
 
 import model.Candidate;
 import store.MemStore;
+import store.PsqlStore;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -12,14 +13,14 @@ import java.io.IOException;
 public class CandidateServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setAttribute("candidates", MemStore.instOf().findAllCandidates());
+        req.setAttribute("candidates", PsqlStore.instOf().findAllCandidates());
         req.getRequestDispatcher("candidate/candidates.jsp").forward(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.setCharacterEncoding("UTF-8");
-        MemStore.instOf().save(new Candidate(Integer.parseInt(req.getParameter("id")),
+        PsqlStore.instOf().save(new Candidate(Integer.parseInt(req.getParameter("id")),
                 req.getParameter("name")));
         resp.sendRedirect(req.getContextPath() + "/candidates.do");
     }
--- src/main/webapp/post/edit.jsp
+++ src/main/webapp/post/edit.jsp
@@ -1,6 +1,6 @@
 <%@ page contentType="text/html; charset=UTF-8" %>
-<%@ page import="store.MemStore" %>
 <%@ page import="model.Post" %>
+<%@ page import="store.PsqlStore" %>
 <!doctype html>
 <html lang="en">
 <head>
@@ -28,7 +28,7 @@
     String id = request.getParameter("id");
     Post post = new Post(0, "");
     if (id != null) {
-        post = MemStore.instOf().findPostById(Integer.parseInt(id));
+        post = PsqlStore.instOf().findPostById(Integer.parseInt(id));
     }
 %>
 <div class="container pt-3">
